7d8b854177a39f53fcdc4985c337ff46ffd57cdc
Merrylyn <africangirl800@gmail.com>
2023-12-13 21:51:39 -0600

    updated all my file codes and their functionality in Insomnia

--- README.md
+++ README.md
@@ -21,3 +21,5 @@ THEN the data for each of these routes is displayed in a formatted JSON
 WHEN I test API POST, PUT, and DELETE routes in Insomnia
 THEN I am able to successfully create, update, and delete data in my database
 ```
+Video Walkthough - https://github.com/M3rycodes/E-COMMERCE-BACKEND.git
+
--- models/index.js
+++ models/index.js
@@ -6,39 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'productTag_products',
   foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'productTag_products',
   foreignKey: 'tag_id'
 })
-// Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
 
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  as: 'productTag_products',
-  foreignKey: 'product_id'
-});
-
-// Tags belongToMany Products (through ProductTag)
 
 module.exports = {
   Product,
